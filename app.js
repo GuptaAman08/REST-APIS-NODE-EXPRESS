34ff6ff584a2b3ba855c2808c4aeae0cfdf2c24d
Aman Gupta <2015aman.a@ves.ac.in>
2020-10-18 20:02:50 +0530

    adding .env for storing jwt secret key and registering all in app.js

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const mongoose = require('mongoose')
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+require('dotenv').config()
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "images")
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + "-" + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if ( file.mimetype === "image/jpeg" || file.mimetype === "image/jpg" || file.mimetype === "image/png" ){
+        // Pass true to accept only images of particuar type else pass false
+        cb(null, true)
+    }else{
+        cb(null, false)
+    }
+}
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(multer({ storage: storage, fileFilter: fileFilter}).single("image"));
+app.use("/images", express.static(path.join(__dirname, "images")))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,5 +42,24 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8080);
+// Express buitl-in special erro handling middleware
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500
+    const message = error.message
+    const data = error.data
+    res.status(status).json({
+        message: message,
+        data: data
+    })
+})
+
+mongoose.connect('mongodb+srv://aman:snZ5L0a4JMbsXqWG@primary.u62r1.mongodb.net/feeds?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true})
+.then(res => {
+    console.log('Connected')
+    app.listen(8080);
+})
+.catch(err => {
+    console.log('Connection Error', err)
+})
