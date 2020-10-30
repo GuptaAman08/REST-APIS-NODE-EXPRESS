4b9629f0b43c110d05cd66b411d2cd5b18dec017
Aman Gupta <2015aman.a@ves.ac.in>
2020-10-30 20:13:57 +0530

    changing name of start file

---
+++ test/sample-test-example.js
@@ -0,0 +1,9 @@
+const expect = require("chai").expect
+
+
+describe('Sample KickStart Test', () => {
+    it("should be equal to 5", () => {
+        let a = 1, b = 4;
+        expect(a+b).to.equal(5)
+    })
+})
